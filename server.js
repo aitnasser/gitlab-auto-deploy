261d377a74f18d7bbcbd7ce00e9e53a7dffa4bf1
m <aitnasser@gmail.com>
2017-02-17 16:38:40 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var gitlabhook = require('gitlabhook');
+var gitlab = gitlabhook({
+	logger: {
+		info: console.log,
+		error: console.error
+	},
+	configPathes: './',
+	port: '3420'
+});
+gitlab.listen();
